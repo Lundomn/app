68e2d9720e9daa93baab420826ceb0513e1917a4
Lundomn <134036682+Lundomn@users.noreply.github.com>
2026-01-23 16:26:45 +0800

    Update app.py

--- app.py
+++ app.py
@@ -207,7 +207,7 @@ if stop_clicked:
 if st.session_state.running:
     # --- SCG 波形设置 ---
     window_size = 1000 
-    step = 30           # 保持大步长
+    step = 10           # 保持大步长
     sleep_time = 0.01   # 保持低延迟
     
     # --- 血压更新设置 ---
@@ -270,3 +270,4 @@ if st.session_state.running:
         
         progress_bar.progress(min(progress_ratio, 1.0))
         time.sleep(sleep_time)
+
