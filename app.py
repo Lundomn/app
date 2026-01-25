6551583739f8bcf375471395fdfe1f508afd2101
Lundomn <134036682+Lundomn@users.noreply.github.com>
2026-01-25 21:00:02 +0800

    Update app.py

--- app.py
+++ app.py
@@ -204,7 +204,7 @@ else:
     if st.session_state.running:
         window = 2000
         step = 50         # 保持不变
-        cycle_duration = 1.5 
+        cycle_duration = 1
         cycle_start = time.time()
         
         # 预编译 Chart 对象
@@ -256,5 +256,6 @@ else:
 
             loop_counter += 1
             time.sleep(0.01)
+
 
 
