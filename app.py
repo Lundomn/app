6d1cca2389a6e59838440ecdb22f6565448ca3ed
Lundomn <134036682+Lundomn@users.noreply.github.com>
2026-01-27 22:06:07 +0800

    Update app.py

--- app.py
+++ app.py
@@ -172,8 +172,8 @@ else:
     if st.session_state.running:
         # 固定序列逻辑
         random.seed(42) 
-        fixed_sbp_seq = [random.randint(130, 132) for _ in range(18)]
-        fixed_dbp_seq = [random.randint(65, 67) for _ in range(18)]
+        fixed_sbp_seq = [random.randint(125, 127) for _ in range(18)]
+        fixed_dbp_seq = [random.randint(68, 70) for _ in range(18)]
         
         window = 2000
         step = 20
@@ -222,6 +222,7 @@ else:
 
             loop_counter += 1
             time.sleep(0.01)
+
 
 
 
